637fb36cb5a10904a87e8713d388dbfb44bc4473
NJ-202107071152\Administrator <wtnl12345@163.com>
2022-08-17 15:21:02 +0800

    代码提交

---
+++ test/tesg.py
@@ -0,0 +1,21 @@
+"""
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+-------------------------------------------------
+# @Project  :PythonWeb
+# @File     :tesg.py
+# @Date     :2022/8/17 14:16
+# @Author   :张铠
+# @IDE :PyCharm
+-------------------------------------------------
+"""
+class DEMO():
+    name = 'title'
+    __weight = '120'
+
+    def __init__(self,name,age,w):
+        self.name = name
+        self.age = age
+        self.__weight = w
+
+x = DEMO()
